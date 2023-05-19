614db11ba7fd8e348a94bb0de5948100bfe00c2e
Ajit <ajitk2805@gmail.com>
2023-05-19 21:17:31 +0530

    model trainer

--- src/utils.py
+++ src/utils.py
@@ -56,8 +56,6 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
             # Train model
             model.fit(X_train,y_train)
 
-            
-
             # Predict Testing data
             y_test_pred =model.predict(X_test)
 
